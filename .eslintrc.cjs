154e150227d871bf0ec4d36725e7f8d13bc9ef78
ngseke <ngseke@gmail.com>
2023-11-24 13:10:36 +0800

    style: fix lints

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        project: ['./tsconfig.json', './tsconfig.node.json'],
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.vue'],
-    },
-    ignorePatterns: ['dist'],
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue']
+  },
+  ignorePatterns: ['dist'],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
 }
